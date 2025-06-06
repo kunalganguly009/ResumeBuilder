534c783a60122ac2033357988f92df9fafe12439
Kunal Ganguly <kunalganguly009@gmail.com>
2025-06-06 20:28:22 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-mongoose
-  .connect(`mongodb+srv://VisualResume:VisualResume@cluster0.jovrj.mongodb.net/?retryWrites=true&w=majority`, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log(`connected to ${process.env.DB_Name} DB`);
+mongoose.set('strictQuery', false);
+mongoose.connect(`mongodb+srv://projectResumeBuilder:resumeBuilderPassword@cluster0.vvtztm1.mongodb.net/resumeBuilderDB?retryWrites=true&w=majority&appName=Cluster0`, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
+.then(() => {
+  console.log(`connected to ${process.env.DB_Name} DB`);
   })
   .catch((e) => {
     console.log(e);
